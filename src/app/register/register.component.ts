1c403f2f0476ddfe6ede2001180fcdde9c5d00b8
aby <abyraaby330@gmail.com>
2022-06-19 02:27:50 +0530

    rective form-register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
 
@@ -17,9 +17,10 @@ export class RegisterComponent implements OnInit {
   //form group
   registerForm = this.fb.group({
     
-    acno:'',
-    pswd:'',
-    uname:''
+    uname:['',[Validators.required,Validators.pattern('[a-zA-Z ]*')]],
+    acno:[''],
+    pswd:['']
+    
   })
 
   constructor(private ds:DataService,private router:Router,private fb:FormBuilder) { }
@@ -29,11 +30,15 @@ export class RegisterComponent implements OnInit {
 
   register(){
 
-  var uname=this.uname
-  var acno=this.acno
-  var pswd=this.pswd
+  var uname=this.registerForm.value.uname
+  var acno=this.registerForm.value.acno
+  var pswd=this.registerForm.value.pswd
 
-const result =this.ds.register(uname,acno,pswd)
+  
+
+  if(this.registerForm.valid){
+
+    const result =this.ds.register(uname,acno,pswd)
 
 if (result){
   alert("successfully registerd")
@@ -42,6 +47,12 @@ if (result){
 else{
   alert("Already existing customer... please login !!!")
 }
-
   }
+else{
+  alert("Invalid Form")
 }
+}
+}
+
+
+
